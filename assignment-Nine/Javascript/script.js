8fbefd1814793f4d5bfae570222d1cc8c2d63c2e
abdul damilola <abduldamilola79@gmail.com>
2025-08-22 22:15:12 +0100

    assignment nine submission

---
+++ assignment-Nine/Javascript/script.js
@@ -0,0 +1,58 @@
+// Declare Name and Age...
+
+let my_name = "Damilola"; // Replace with actual name 
+let age = "40"; // Replace with actual age 
+
+console.log(my_name);
+console.log(age);
+
+// Declare Variables....
+
+// Using Const for values that is won't change 
+const full_name = "Abdul Damilola"
+const favorite_city = "Baltimore MD"
+
+console.log(full_name); // Fullname
+console.log(favorite_city); // Favoritecity
+
+// Using let  for values that might change
+let Age = 28; // age
+let isStudent = true; // whether you are a student (true/false)
+
+console.log(age);
+console.log(isStudent);
+
+// String Concatenation vs Template Literals..
+
+// Declare variable for name and city
+ const name = "damilola";
+ const city = "Abuja"
+
+ // Using String Concatenation
+
+ console.log("My name is " + name + " and I live in " + city +".");
+
+// Using Templates Literals
+
+console.log(`My name is ${name} and I live in ${city}.`);
+
+//Primitive types
+
+// String type
+let myname = "Jarule";
+// Number type
+let myage = 50;
+// Boolean type
+let isClass = true; 
+// Undefined type
+let location;
+// Null type
+let school = null;
+
+console.log(typeof myname);// string
+console.log(typeof myage);// number
+console.log(typeof isClass);// Boolean
+console.log(typeof location);// Undefined
+console.log(typeof school); // Null
+
+
